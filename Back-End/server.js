768a0ffe2ac0f990854e6915b7ea0b351134f87f
MahanShetty004 <mahanshetty488@gmail.com>
2025-09-14 01:28:07 +0530

    Student Feedback and finding all student feedback done

--- Back-End/server.js
+++ Back-End/server.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion ,ObjectId} = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const dotenv = require("dotenv");
 const bcrypt = require("bcryptjs");
 const validator = require("validator");
@@ -28,6 +28,10 @@ class Feedback_Database {
             this.database = this.client.db('StudentfeedbackSystem');
             await this.database.collection('Student').createIndex({ "std_email": 1 }, { unique: true });
             await this.database.collection('Admin').createIndex({ "ad_email": 1 }, { unique: true });
+            await this.database.collection('Feedback').createIndex(
+                { std_id: 1, course_id: 1 },
+                { unique: true }
+            );
             console.log("Successfully connected to MongoDB Atlas!");
             return this.client;
 
@@ -60,11 +64,12 @@ class Feedback_Database {
                 throw new Error("Password must be at least 8 characters long and contain at least 1 number and 1 special character.");
             }
             const hashedPassword = await bcrypt.hash(password, 10);
-            await this.database.collection('Student').insertOne({
+            const result = await this.database.collection('Student').insertOne({
                 std_name: user_name,
                 std_email: email,
                 password: hashedPassword
             });
+            return result.insertedId;
         }
         catch (error) {
             if (error.code == 11000) {
@@ -107,7 +112,7 @@ class Feedback_Database {
             throw error;
         }
     }
-    async Student_Login_check(email,password) {
+    async Student_Login_check(email, password) {
         try {
             const user = await this.database.collection('Student').findOne({ std_email: email });
             if (!user) {
@@ -128,7 +133,7 @@ class Feedback_Database {
             throw error;
         }
     }
-    async Admin_Login_check(email,password) {
+    async Admin_Login_check(email, password) {
         try {
             const user = await this.database.collection('Admin').findOne({ ad_email: email });
             if (!user) {
@@ -151,19 +156,23 @@ class Feedback_Database {
     }
     async Insert_Courses(adminId, course) {
         try {
-            await this.database.collection('Course').insertOne({
+            if (!ObjectId.isValid(adminId)) {
+                console.error("Error: The provided Admin ID is not a valid ObjectId.");
+                throw new Error("Invalid Admin ID.");
+            }
+            const result = await this.database.collection('Course').insertOne({
                 course: course,
                 ad_id: adminId,
             });
+            return result.insertedId;
         }
         catch (error) {
             console.error("An unexpected error occurred during insertion:", error);
             throw error;
         }
     }
-    async Delete_Course(courseId)
-    {
-        try{
+    async Delete_Course(courseId) {
+        try {
             if (!ObjectId.isValid(courseId)) {
                 console.error("Error: The provided course ID is not a valid ObjectId.");
                 throw new Error("Invalid course ID.");
@@ -180,9 +189,48 @@ class Feedback_Database {
                 return { success: false, message: `No course found with ID: ${courseId}.` };
             }
         }
+        catch (error) {
+            console.error("An unexpected error occurred during deletion:", error);
+            throw error;
+        }
+    }
+    async Insert_Feedback(Student_id, course_id, rating, message) {
+        try {
+            if (!ObjectId.isValid(Student_id)) {
+                console.error("Error: The provided Student ID is not a valid ObjectId.");
+                throw new Error("Invalid Student ID.");
+            }
+            if (!ObjectId.isValid(course_id)) {
+                console.error("Error: The provided course ID is not a valid ObjectId.");
+                throw new Error("Invalid course ID.");
+            }
+            await this.database.collection('Feedback').insertOne({
+                std_id: Student_id,
+                course_id: course_id,
+                rating: rating,
+                comment: message
+            });
+        }
+        catch (error) {
+            if(error.code===11000)
+            {
+                console.error("Error: Feedback alredy given");
+            }
+            else{
+                console.error("An unexpected error occurred during insertion:", error);
+            }
+            throw error;
+        }
+    }
+    async Student_Feedback(st_id)
+    {
+        try{
+            const result=await this.database.collection('Feedback').find({std_id:st_id}).toArray();
+            return result;
+        }
         catch(error)
         {
-            console.error("An unexpected error occurred during deletion:", error);
+            console.error("An unexpected error occurred during finding the feedback:", error);
             throw error;
         }
     }
@@ -190,15 +238,20 @@ class Feedback_Database {
 (async () => {
     const db = new Feedback_Database(process.env.MONGODB_URL);
     await db.connectToDatabase();
-    // await db.Insert_Student("Mahan", "abcdefgh1@", "mahanshetty488@gmail.com");
-    // const adm_id = await db.Insert_Admin("MAHANJ", "abcdefgh1@", "mahanshetty488@gmail.com");
-    // await db.Insert_Courses(adm_id, "Math");
-    // const stdlogin=await db.Student_Login_check("mahanshetty488@gmail.com","abcdefgh1@");
+    const st_id = await db.Insert_Student("Mahan", "abcdefgh1@", "mahanshetty488@gmail.com");
+    const adm_id = await db.Insert_Admin("MAHANJ", "abcdefgh1@", "mahanshetty488@gmail.com");
+    const c_id = await db.Insert_Courses(adm_id, "Math");
+    const c_id2 = await db.Insert_Courses(adm_id, "Science");
+    // const stdlogin = await db.Student_Login_check("mahanshetty488@gmail.com", "abcdefgh1@");
     // console.log(stdlogin);
-    // const adlogin=await db.Admin_Login_check("mahanshetty488@gmail.com","abcdefgh1@");
+    // const adlogin = await db.Admin_Login_check("mahanshetty488@gmail.com", "abcdefgh1@");
     // console.log(adlogin);
-    const deletedcourses=await db.Delete_Course('68c5abcd803a4b1a29fcab6b');
-    console.log(deletedcourses);
+    // const deletedcourses=await db.Delete_Course('68c5abcd803a4b1a29fcab6b');
+    // console.log(deletedcourses);
+    await db.Insert_Feedback(st_id,c_id,4,"Great course");
+    await db.Insert_Feedback(st_id,c_id2,5,"Great course");
+    const std_feedback=await db.Student_Feedback(st_id);
+    console.log(std_feedback);
     await db.closeConnection();
 })();
 module.exports = { Feedback_Database };
